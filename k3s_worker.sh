b946192ade3c556136e6c7700bd02c3ddcb01c08
mbani01 <mouad-mb@outlook.com>
2022-11-20 16:46:35 +0100

    server config done/ should config kubectl in agent

--- k3s_worker.sh
+++ k3s_worker.sh
@@ -5,5 +5,6 @@ server_url="https://${server_ip}:6443"
 sudo touch /etc/profile.d/vars.sh
 echo "export K3S_TOKEN_FILE=${k3s_token_file}" >> /etc/profile.d/vars.sh
 echo "export K3S_URL=${server_url}" >> /etc/profile.d/vars.sh
-export INSTALL_K3S_EXEC="--flannel-iface=eth1"
-curl -sfL https://get.k3s.io | sh - && echo "K3s Agent is Running ....................>_<"
+echo "sudo mkdir -p $HOME/.kube" >> /etc/profile.d/vars.sh
+echo "sudo cp /vagrant_data/config $HOME/.kube/" >> /etc/profile.d/vars.sh
+curl -sfL https://get.k3s.io | K3S_URL=${server_url} K3S_TOKEN_FILE=${k3s_token_file}  INSTALL_K3S_EXEC="--flannel-iface=eth1" sh - && echo "K3s Agent is Running ....................>_<"
