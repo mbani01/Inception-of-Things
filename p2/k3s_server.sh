fc90824fdc226c09e22c331bbc1dcc4c6fd0be97
mbani01 <mouad-mb@outlook.com>
2022-11-27 17:39:53 +0100

    add kubectl alias

--- p2/k3s_server.sh
+++ p2/k3s_server.sh
@@ -2,3 +2,4 @@ server_ip=$1
 
 export INSTALL_K3S_EXEC="--bind-address=$server_ip --flannel-iface=eth1"
 curl -sfL https://get.k3s.io | sh - && echo "K3s Server is Running ....................>_<"
+echo "alias k=\"sudo kubectl\"" >> /etc/profile
