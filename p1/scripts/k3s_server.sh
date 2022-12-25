4b38542615d7d52575354da29e2bd181f1e4861d
mbani01 <mouad-mb@outlook.com>
2022-12-25 11:41:12 +0100

    update config path

--- p1/scripts/k3s_server.sh
+++ p1/scripts/k3s_server.sh
@@ -2,6 +2,6 @@ server_ip=$1
 
 export INSTALL_K3S_EXEC="--bind-address=$server_ip --flannel-iface=eth1"
 curl -sfL https://get.k3s.io | sh - && echo "K3s Server is Running ....................>_<"
-sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/server_token
-sudo cp /etc/rancher/k3s/k3s.yaml /vagrant/config
+sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/confs/server_token
+sudo cp /etc/rancher/k3s/k3s.yaml /vagrant/confs/config
 echo "alias k=\"sudo kubectl\"" >> /etc/profile
--- p1/scripts/k3s_worker.sh
+++ p1/scripts/k3s_worker.sh
@@ -1,11 +1,12 @@
 server_ip=$1
 
-k3s_token_file="/vagrant/server_token"
+k3s_token_file="/vagrant/confs/server_token"
 server_url="https://${server_ip}:6443"
 sudo touch /etc/profile.d/vars.sh
 echo "export K3S_TOKEN_FILE=${k3s_token_file}" >> /etc/profile.d/vars.sh
 echo "export K3S_URL=${server_url}" >> /etc/profile.d/vars.sh
 echo "sudo mkdir -p /home/vagrant/.kube" >> /etc/profile.d/vars.sh
-echo "sudo cp /vagrant/config /home/vagrant/.kube/" >> /etc/profile.d/vars.sh
+echo "sudo cp /vagrant/confs/config /home/vagrant/.kube/" >> /etc/profile.d/vars.sh
 echo "alias k=\"sudo kubectl\"" >> /etc/profile.d/vars.sh
 curl -sfL https://get.k3s.io | K3S_URL=${server_url} K3S_TOKEN_FILE=${k3s_token_file}  INSTALL_K3S_EXEC="--flannel-iface=eth1" sh - && echo "K3s Agent is Running ....................>_<"
+sudo rm /vagrant/confs/*
