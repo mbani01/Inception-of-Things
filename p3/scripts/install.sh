7bc94cb8b6610f5a39025b4a6b5d830baddc6346
mbani01 <mouad-mb@outlook.com>
2022-12-31 18:29:48 +0100

    install kubectl

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -1,15 +1,20 @@
-sudo apt-get -y remove docker docker-engine docker.io containerd runc
-sudo apt-get -y update
-sudo apt-get -y install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get -y update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo docker run hello-world
+# #install Docker
+# sudo apt-get -y remove docker docker-engine docker.io containerd runc
+# sudo apt-get -y update
+# sudo apt-get -y install \
+#     ca-certificates \
+#     curl \
+#     gnupg \
+#     lsb-release
+# sudo mkdir -p /etc/apt/keyrings
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# echo \
+#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+#   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# sudo apt-get -y update
+# sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+# sudo docker run hello-world
+#incstall kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.18.0/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
