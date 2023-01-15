a87a1e6b041da092d3f2a8ea4dc418d1ccfc7421
mbani01 <mouad-mb@outlook.com>
2023-01-15 23:17:22 +0100

    edit script

--- p3/scripts/k3d.sh
+++ p3/scripts/k3d.sh
@@ -1,5 +1,5 @@
 # Create a k3d cluster
-k3d cluster create iot
+k3d cluster create iot --api-port 6443 -p 8080:80@loadbalancer --agents 2
 # argocd Configuration
-sudo kubectl create namespace argocd
-sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+# sudo kubectl create namespace argocd
+# sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
